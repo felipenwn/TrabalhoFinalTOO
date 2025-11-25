8ae0c6b29fedb87d30ba0568efab2c371ef42f13
felipenwn <felipenwns@gmail.com>
2025-11-24 23:54:13 -0300

    add consulta

--- Consulta.py
+++ Consulta.py
@@ -1,18 +1,33 @@
-class Consulta:
-    def __init__(self,data,hora,descricao,status):
-        self.data = data
-        self.hora = hora
-        self.descricao = descricao
-        self.status = status
 
-    def agendarConsulta(self,data,hora,descricao):
-        self.data = data
-        self.hora = hora
-        self.descricao = descricao
-        self.status = "Agendada"
+class Consulta:
+    def __init__(self, data, hora, descricao, animal, veterinario, status="Agendada"):
+        self._data = data
+        self._hora = hora
+        self._descricao = descricao
+        self._animal = animal
+        self._veterinario = veterinario
+        self._status = status
+        
+    @property
+    def animal(self):
+        return self._animal
+    
+    @property
+    def veterinario(self):
+        return self._veterinario
+    
+   
+    def agendarConsulta(self, data, hora, descricao):
+        self._data = data
+        self._hora = hora
+        self._descricao = descricao
+        self._status = "Agendada"
         
     def cancelarConsulta(self):
-        self.status = "Cancelada"
+        self._status = "Cancelada"
 
     def finalizarConsulta(self):
-        self.status = "Finalizada" 
+        self._status = "Finalizada"
+        
+    def __str__(self):
+        return f"Consulta em {self._data} {self._hora}. Status: {self._status}. Vet: {self._veterinario.nome}"
